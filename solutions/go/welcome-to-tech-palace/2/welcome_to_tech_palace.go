64adf4012b6b212d19a703fb3424b76f0ad65164
root <root@ip-10-1-0-72.eu-west-2.compute.internal>
2025-06-05 14:44:29 +0000

    [Sync Iteration] go/welcome-to-tech-palace/2

---
+++ solutions/go/welcome-to-tech-palace/2/welcome_to_tech_palace.go
@@ -0,0 +1,23 @@
+package techpalace
+
+import "strings"
+
+// WelcomeMessage returns a welcome message for the customer.
+func WelcomeMessage(customer string) string {
+	message := "Welcome to the Tech Palace, " + strings.ToUpper(customer) 
+    return message
+}
+
+// AddBorder adds a border to a welcome message.
+func AddBorder(welcomeMsg string, numStarsPerLine int) string {
+	border := strings.Repeat("*", numStarsPerLine)
+    messageWithBorder := border + "\n" + welcomeMsg + "\n" + border
+    return messageWithBorder
+}
+
+// CleanupMessage cleans up an old marketing message.
+func CleanupMessage(oldMsg string) string {
+    removeStars := strings.ReplaceAll(oldMsg, "*", "")
+	trimmedMessage := strings.TrimSpace(removeStars)
+    return trimmedMessage
+}
